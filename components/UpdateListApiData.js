59d6b50c734fcb6a9b99436dd4886d1395aae2aa
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-08-09 11:12:29 +0530

    create modal

--- components/UpdateListApiData.js
+++ components/UpdateListApiData.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Button, StyleSheet, Text, View, Modal, Alert } from 'react-native'
 
 const UpdateListApiData = () => {
 
     const [data, setData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(undefined)
 
     const getApiData = async () => {
         const url = "http://10.0.2.2:3000/users";
@@ -26,9 +28,14 @@ const UpdateListApiData = () => {
         }
     }
 
-    useState(() => {
+    useEffect(() => {
         getApiData();
     }, [])
+
+    const updateUser = (data) => {
+        setShowModal(true)
+        setSelectedUser(data)
+    }
 
     return (
         <View style={styles.container}>
@@ -38,19 +45,40 @@ const UpdateListApiData = () => {
                 <View style={{ flex: 2 }}><Text>Age</Text></View>
                 <View style={{ flex: 2 }}><Text>Operations</Text></View>
             </View>
+
             {
                 data.length ?
-                    data.map((item) => <View style={styles.dataWrapper}>
-                        <View style={{ flex: 1 }}><Text>{item.name}</Text></View>
-                        <View style={{ flex: 1 }}><Text>{item.age}</Text></View>
-                        <View style={{ flex: 1, marginHorizontal: 5 }}><Button onPress={() => deleteUser(item.id)} title='Delete' color={"red"} /></View>
-                        <View style={{ flex: 1 }}><Button title='Update' /></View>
-                    </View>)
+                    data.map((item) => (
+                        <View key={item.id} style={styles.dataWrapper}>
+                            <View style={{ flex: 1 }}><Text>{item.name}</Text></View>
+                            <View style={{ flex: 1 }}><Text>{item.age}</Text></View>
+                            <View style={{ flex: 1, marginHorizontal: 5 }}>
+                                <Button onPress={() => deleteUser(item.id)} title='Delete' color={"red"} />
+                            </View>
+                            <View style={{ flex: 1 }}>
+                                <Button title='Update' onPress={() => updateUser(item)} />
+                            </View>
+                        </View>
+                    ))
                     : null
             }
+            <Modal visible={showModal} transparent={true}>
+                <UserModal setShowModal={setShowModal} selectedUser={selectedUser} />
+            </Modal>
         </View>
     )
 }
+
+const UserModal = (props) => {
+    return (
+        <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                <Text>{props.selectedUser.name}</Text>
+                <Button title='Close' onPress={() => props.setShowModal(false)} />
+            </View>
+        </View>
+    )
+}  
 
 const styles = StyleSheet.create({
     container: {
@@ -65,6 +93,19 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         margin: 5,
         padding: 8,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    modalView: {
+        backgroundColor: 'white',
+        padding: 60,
+        borderRadius: 10,
+        shadowColor: '#000',
+        elevation: 5,
+        shadowOpacity: 0.70
     }
 })
 
